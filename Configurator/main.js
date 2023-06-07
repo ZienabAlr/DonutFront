f2bc40878697b6ec319025a34feac1844d3442b8
ZienabAlr <r0784494@student.thomasmore.be>
2023-06-07 15:50:18 +0200

    feat(main.js): function animate added and scene is renderd

--- Configurator/main.js
+++ Configurator/main.js
@@ -13,3 +13,9 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
