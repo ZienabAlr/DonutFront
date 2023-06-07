6c88e38630f85b1cb4b1e01580d814365afd8458
ZienabAlr <r0784494@student.thomasmore.be>
2023-06-07 16:21:55 +0200

    feat(main.js): gltf loader has been imported and 3D model has been added into the scene

--- Configurator/main.js
+++ Configurator/main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+//import GLTFLoader 
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -9,16 +11,21 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-//controls.update() must be called after any manual changes to the camera's transform
-//camera.position.set(0, 20, 100);
 //make the background transparent
 renderer.setClearColor(0x000000, 0);
-
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+//add gltf model
+let donut;
+const loader = new GLTFLoader();
+loader.load('models/donut1.glb', function (gltf) {
+  scene.add(gltf.scene);
+  donut = gltf.scene;
+});
 
 camera.position.z = 5;
 
