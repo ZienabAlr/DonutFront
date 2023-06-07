125b0a0badeba791221b65d98e455a9419437591
ZienabAlr <r0784494@student.thomasmore.be>
2023-06-08 00:48:06 +0200

    feat(postOrder.js): function with post request is added

--- Configurator/postOrder.js
+++ Configurator/postOrder.js
@@ -1,6 +1,50 @@
 export default class Order{
 
     //post order to server
+    postOrder(){
+        //get form values
+        const company = document.querySelector('.company');
+        const email = document.querySelector('.email');
+        const phone = document.querySelector('.phone');
+        const donutname = document.querySelector('.donutname');
+        const glaze = document.querySelector('.glaze');
+        const amount = document.querySelector('.amount');
+        const notes = document.querySelector('.note');
+        
+        //create order object
+        const order = {
+            company: company.value,
+            email: email.value,
+            phone: phone.value,
+            donutname: donutname.value,
+            glaze: glaze.value,
+            amount: amount.value,
+            notes: notes.value
+        }
+        //post order to server
+        fetch('http://localhost:3000/api/v1/donutello', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        }).then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+
+            company.value = '';
+            email.value = '';
+            phone.value = '';
+            donutname.value = '';
+            glaze.value = '';
+            amount.value = '';
+            notes.value = '';
+
+        })
+    }
+   
+  
+
    
 }
 
