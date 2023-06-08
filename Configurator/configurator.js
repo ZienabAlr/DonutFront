d9d5d8211b23db1b49db866f223592c38ff748db
ZienabAlr <r0784494@student.thomasmore.be>
2023-06-08 10:21:43 +0200

    fix(configurator.js): the model is rendered inside the div canvas and the size of the renderer has been edited

--- Configurator/configurator.js
+++ Configurator/configurator.js
@@ -7,9 +7,12 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+const model = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+// document.body.appendChild(renderer.domElement);
+model.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 //make the background transparent
